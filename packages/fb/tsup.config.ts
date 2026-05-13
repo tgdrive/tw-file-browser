eefe85349aab502c6cdca18036024e8ab8df7061
divyam234 <47589864+divyam234@users.noreply.github.com>
2026-05-13 16:06:44 +0530

    chore: update tsup config - externalize react deps

--- packages/fb/tsup.config.ts
+++ packages/fb/tsup.config.ts
@@ -1,16 +1,22 @@
 import { defineConfig } from "tsup";
 import Icons from "unplugin-icons/esbuild";
+
 export default defineConfig({
   clean: true,
   target: "es2019",
   format: ["esm"],
   treeshake: true,
-  dts: true,
   entry: ["src/index.ts"],
+  dts: {
+    compilerOptions: {
+      noUnusedLocals: false,
+    },
+  },
   esbuildPlugins: [
     Icons({
       compiler: "jsx",
       jsx: "react",
     }),
   ],
+  external: ["react", "react-dom"],
 });
